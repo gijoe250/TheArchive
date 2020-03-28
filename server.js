44be6901ab53a9011179d307eaa55b85eb66e228
Joe Garcia <gijoe250@gmail.com>
2020-03-28 16:26:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const configDB = require('./config/database');
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 3500;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
