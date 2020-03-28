47d2bdf61dc695a37d26b8f6377031158fa921a4
Joe Garcia <gijoe250@gmail.com>
2020-03-28 13:04:23 -0500

    password change
    poop

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // CHANGE <username> && <password> to your mongoDB user info
 module.exports = {
-    uri: 'mongodb+srv://<username>:<password>@cluster0-oddgf.azure.mongodb.net/test?retryWrites=true&w=majority'
+    uri: 'mongodb+srv://stealing:test@cluster0-oddgf.azure.mongodb.net/test?retryWrites=true&w=majority'
 };
 
