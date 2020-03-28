f73d7c34af0bc769a4054bb48610dda15fd6fc3b
bml419 <47117598+bml419@users.noreply.github.com>
2020-03-28 16:37:52 -0500

    test

--- app/routes/boardgames.js
+++
@@ -1,3 +0,0 @@
-var mongoose = require('mongooose');
-
-mongoose.connect(mongodb+srv://stealing:test@cluster0-oddgf.azure.mongodb.net/test?retryWrites=true&w=majority)
--- config/database.js
+++
@@ -1,6 +0,0 @@
-// CHANGE <username> && <password> to your mongoDB user info
-module.exports = {
-    uri: 'mongodb+srv://stealing:test@cluster0-oddgf.mongodb.net/test?retryWrites=true&w=majority'
-
-};
-
