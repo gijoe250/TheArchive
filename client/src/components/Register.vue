aae0578aac7a27a3c881685bcc2f9eafdd4566c3
IvyBrandyn <IvyBrandyn@users.noreply.github.com>
2020-03-29 02:30:59 -0500

    Finessing Login and register

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class = "container">
-        <div class="card" style="width: 30rem;">
-            <div class="card border-dark mb-3" style="max-width: 30rem;">
+    <div>
+        <div class="card horizontally-aligned" style="width: 30rem;">
+            <div class="card" id="card" style="max-width: 30rem;">
                 <div class="card bg-dark text-white">
                     <div class="card-header"><h3>Register</h3>
                     </div>
@@ -9,13 +9,30 @@
                     <div class="card-body">
                         <div align="center"><font size="4">{{ thing }}</font></div>
                         <form>
-                            <font size="4">Username</font><input v-model="username" placeholder="Enter Username" @keyup.enter="authorize">
-                            <br>
-                            <font size="4">Password</font><input type="password" v-model="password" placeholder="Enter Password" @keyup.enter="authorize">
-                            <br>
-                            <font size="4">Verify Password</font><input type="password" v-model="password2" placeholder="Verify Password" @keyup.enter="authorize">
-                            <br>
-                            <br>
+                            <div class="row">
+                                <div class="col">
+                                    <font size="4">Username</font>
+                                </div>
+                                <div class="col">
+                                    <input v-model="username" placeholder="Enter Username" @keyup.enter="authorize">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <font size="4">Password</font>
+                                </div>
+                                <div class="col">
+                                <input type="password" v-model="password" placeholder="Enter Password" @keyup.enter="authorize">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                   <font size="4">Verify Password</font>
+                                </div>
+                                <div class="col">
+                                    <input type="password" v-model="password2" placeholder="Verify Password" @keyup.enter="authorize">
+                                </div>
+                            </div>
                             <div class="float-right">
                             <b-button @click="authorize">Submit</b-button>
                             </div>
@@ -26,11 +43,16 @@
                     </div>
                 </div>
             </div>
+        <div id="response" class="response-message horizontally-aligned">
+            <p>You have registered succesfully!</p>
+        </div>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+import $ from 'jquery';
 export default {
     name: 'register',
     data() {
@@ -49,13 +71,41 @@ export default {
                 }
             else {
                 this.thing = ''
+                $("#card").slideUp();
+                $("#response").slideDown();
                 }
             this.password = ''
             this.password2 = ''
+
+        },
+        register: function() {
+        let registerInfo = {
+            username: this.username,
+            password: this.password
+        };
+
+        axios
+            .post('http://localhost:8081/api/post/', registerInfo)
+            .then(response => {
+            console.log(response);
+            })
+            .catch(error => {
+            console.log(error);
+            });
         }
+
     }
 };
 </script>
 
 <style>
+.horizontally-aligned{
+    margin-bottom: 0;
+    margin-top: 0;
+    margin-left: auto;
+    margin-right: auto;
+}
+.response-message{
+    display: none;
+}
 </style>
