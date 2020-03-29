bcb5aea7a850d0e12b21b860090f522da6ffaba0
Joe Garcia <gijoe250@gmail.com>
2020-03-28 20:27:14 -0500

    All changes to route and db

---
+++ api/routes/boardgames.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Boardgame = require('../models/boardgame');
+
+//@route        GET api/profile
+//@description  Test route
+//@access       Public
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET reqs to /boardgames'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const boardgame = new Boardgame({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+    boardgame.save();
+    res.status(201).json({
+        message: 'Handling POST reqs to /boardgames',
+        boardgame: boardgame
+    });
+});
+
+router.get('/:boardgameId', (req, res, next) => {
+    const id = req.params.boardgameId;
+});
+
+router.patch('/:boardgameId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated'
+    });
+});
+
+router.delete('/:boardgameId', (req, res, next) => {
+    res.status(200).json({
+        message: 'delete'
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,8 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+const http = require('http');
+const app = require('./app');
 
-const configDB = require('./config/database');
+const port = process.env.PORT || 3000;
 
-mongoose.connect(
-    configDB.uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-            //dbName: process.env.DATABASE_NAME
-    },
-    function(err) {
-        if (err) throw err;
-        console.log('Sucesssfully connected');
-    }
-);
+const server = http.createServer(app);
 
-// Middle ware
-app.use(express.json({ extended: false }));
-app.use(bodyParser.json());
-//app.set('view engine', 'ejs'); // set up ejs for templating
-
-app.use('/api/items', require('./app/routes/items.js'));
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server Started on ${PORT}'));
+server.listen(port);
