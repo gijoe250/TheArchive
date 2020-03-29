f5dc815513e88d52ade1445b6d070f8efa2979ba
jnguyen <zfv431@my.utsa.edu>
2020-03-28 22:13:33 -0500

    fix error messages

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,15 +1,20 @@
 <template>
-    <div>
-        Login
-        <br>
-        <input v-model="username" id="un" placeholder="Username" @keyup.enter="authorize">
-        <br>
-        <input type="password" id="pw" placeholder="Password">
-        <br>
-        <button @click="authorize">
-        Go
-        </button>
-    </div>
+  <div>
+    Login
+    <br />
+    <input
+      v-model="username"
+      id="un"
+      placeholder="Username"
+      @keyup.enter="authorize"
+    />
+    <br />
+    <input type="password" id="pw" placeholder="Password" />
+    <br />
+    <button @click="authorize">
+      Go
+    </button>
+  </div>
 </template>
 
 <script>
@@ -17,24 +22,24 @@ import $ from 'jquery';
 
 export default {
   name: 'login',
-data: {
-  	username: '',
-    password: ''
-    },
-methods: {
-  	authorize: function(){
-    	this.username = ''
-      this.password = ''
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    authorize: function() {
+      this.username = '';
+      this.password = '';
     }
-}
+  }
 };
 
-$( "#pw" ).keydown(function(e) {
-	if(e.which == 13) {
-  			$("#un").val("");
-        $(this).val("");
-    }
+$('#pw').keydown(function(e) {
+  if (e.which == 13) {
+    $('#un').val('');
+    $(this).val('');
+  }
 });
-
-
 </script>
