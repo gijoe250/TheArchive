ef35c313f336e999c007b6396d3c6deb8a5e5d11
IvyBrandyn <IvyBrandyn@users.noreply.github.com>
2020-03-29 00:48:46 -0500

    Login stuff
    and register

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,16 +1,29 @@
 <template>
-    <div>
-        <h>Login</h>
-        <br>
-        <input v-model="username" placeholder="Username" @keyup.enter="authorize">
-        <br>
-        <input type="password" v-model="password" placeholder="Password" @keyup.enter="authorize">
-        <br>
-        <button @click="authorize">
-        Go
-        </button>
-        <br>
-        <router-link to="/register" class="">Sign up</router-link>
+    <div class = "container">
+        <div class="card" style="width: 20rem;">
+            <div class="card border-dark mb-3" style="max-width: 20rem;">
+                <div class="card bg-dark text-white">
+                    <div class="card-header"><h3>Login</h3>
+                    </div>
+                    <br>
+                    <div class="card-body">
+                        <form>
+                            <input v-model="username" placeholder="Username" @keyup.enter="authorize">
+                            <br>
+                            <input type="password" v-model="password" placeholder="Password" @keyup.enter="authorize">
+                            <br>
+                            <br>
+                            <div class="float-right">
+                            <b-button @click="authorize">Go</b-button>
+                            </div>
+                            <br>
+                            <br>
+                            <font size="4">Don't have an account? <router-link to="/register" class="">Sign up</router-link></font> 	
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,18 +1,32 @@
 <template>
-    <div>
-        <div>
-            {{ thing }}
+    <div class = "container">
+        <div class="card" style="width: 30rem;">
+            <div class="card border-dark mb-3" style="max-width: 30rem;">
+                <div class="card bg-dark text-white">
+                    <div class="card-header"><h3>Register</h3>
+                    </div>
+                    <br>
+                    <div class="card-body">
+                        <div align="center"><font size="4">{{ thing }}</font></div>
+                        <form>
+                            <font size="4">Username</font><input v-model="username" placeholder="Enter Username" @keyup.enter="authorize">
+                            <br>
+                            <font size="4">Password</font><input type="password" v-model="password" placeholder="Enter Password" @keyup.enter="authorize">
+                            <br>
+                            <font size="4">Verify Password</font><input type="password" v-model="password2" placeholder="Verify Password" @keyup.enter="authorize">
+                            <br>
+                            <br>
+                            <div class="float-right">
+                            <b-button @click="authorize">Submit</b-button>
+                            </div>
+                            <br>
+                            <br>
+                            <font size="4">Have an account? <router-link to="/login" class="">Login</router-link></font> 	
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <h>Register</h>
-        <br>
-        Username <input v-model="username" placeholder="Enter Username" @keyup.enter="authorize">
-        <br>
-        Password <input type="password" v-model="password" placeholder="Enter Password" @keyup.enter="authorize">
-        <br>
-        Re-enter Password <input type="password" v-model="password2" placeholder="Enter Password" @keyup.enter="authorize">
-        <br>
-        <button @click="authorize">Submit</button>
-        <br>
     </div>
 </template>
 
@@ -43,4 +57,5 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+</style>
